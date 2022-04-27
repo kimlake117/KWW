0269e9316002b7c31df48d3bcd3344633ce71d67
Kim Lake <kim.a.lake117@gmail.com>
2022-04-27 12:06:00 -0500

    Added dynamic account name to nave bar

--- FrontEnd/KWW/KimsWoodWorking/Global.asax.cs
+++ FrontEnd/KWW/KimsWoodWorking/Global.asax.cs
@@ -19,5 +19,8 @@ namespace KimsWoodWorking
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        public static string UserName { get; set; } = "Account";            
     }
 }
+
