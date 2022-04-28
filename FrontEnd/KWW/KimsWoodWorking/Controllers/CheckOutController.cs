b0b161e1fcff854577c9bfe6be4952b5817b95d9
Kim Lake <kim.a.lake117@gmail.com>
2022-04-28 16:32:55 -0500

    created checkout page

---
+++ FrontEnd/KWW/KimsWoodWorking/Controllers/CheckOutController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KimsWoodWorking.Models;
+
+namespace KimsWoodWorking.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        // GET: CheckOut
+        public ActionResult Index()
+        {
+            List<UserCartItemModel> userCart = SqliteDataAccess.getUserCart();
+
+            double total = SqliteDataAccess.UserCartTotalValue();
+
+            ViewBag.TotalCartPrice = total;
+
+            return View(userCart); 
+        }
+    }
+}
